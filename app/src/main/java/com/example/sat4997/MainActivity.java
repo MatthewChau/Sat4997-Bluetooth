b1ba52533467983cbb1c8510d1ec9ad29a7587d2
Matthew <MatthewChauA@gmail.com>
2022-03-23 04:45:48 -0400

    Basic Bluetooth

--- app/src/main/java/com/example/sat4997/MainActivity.java
+++ app/src/main/java/com/example/sat4997/MainActivity.java
@@ -1,20 +1,82 @@
 package com.example.sat4997;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.sat4997.MESSAGE";
 
+    TextView textview2;
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter btAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview2 = (TextView) findViewById(R.id.textView2);
+
+        // Getting the Bluetooth adapter
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        textview2.append("\nAdapter: " + btAdapter);
+
+        CheckBluetoothState();
+    }
+
+    /** Called when an activity completes. */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            CheckBluetoothState();
+        }
+    }
+
+    private void CheckBluetoothState() {
+        // Checks for the Bluetooth support and then makes sure it is turned on
+        // If it isn't turned on, request to turn it on
+        // List paired devices
+        if (btAdapter == null) {
+            textview2.append("\nBluetooth NOT supported. Aborting.");
+            return;
+        } else {
+            if (btAdapter.isEnabled()) {
+                textview2.append("\nBluetooth is enabled...");
+
+                // Listing paired devices
+                textview2.append("\nPaired Devices are:");
+                Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
+                for (BluetoothDevice device : devices) {
+                    textview2.append("\n  Device: " + device.getName() + ", " + device);
+                }
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 
     /** Called when the user taps the Send button */
