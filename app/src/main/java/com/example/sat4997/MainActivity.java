1b5eb4943db6a668bbfd436c7438232b012c5fba
Matthew <MatthewChauA@gmail.com>
2022-03-23 17:46:37 -0400

    Prepare for BLE scanning

--- app/src/main/java/com/example/sat4997/MainActivity.java
+++ app/src/main/java/com/example/sat4997/MainActivity.java
@@ -6,14 +6,23 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         textview2.append("\nAdapter: " + btAdapter);
 
-        CheckBluetoothState();
+        //CheckBluetoothState();
     }
 
     /** Called when an activity completes. */
@@ -43,32 +52,128 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_ENABLE_BT) {
-            CheckBluetoothState();
+            //CheckBluetoothState();
         }
     }
 
-    private void CheckBluetoothState() {
-        // Checks for the Bluetooth support and then makes sure it is turned on
-        // If it isn't turned on, request to turn it on
-        // List paired devices
-        if (btAdapter == null) {
-            textview2.append("\nBluetooth NOT supported. Aborting.");
-            return;
-        } else {
-            if (btAdapter.isEnabled()) {
-                textview2.append("\nBluetooth is enabled...");
+//    private void CheckBluetoothState() {
+//        // Checks for the Bluetooth support and then makes sure it is turned on
+//        // If it isn't turned on, request to turn it on
+//        // List paired devices
+//        if (btAdapter == null) {
+//            textview2.append("\nBluetooth NOT supported. Aborting.");
+//            return;
+//        } else {
+//            if (btAdapter.isEnabled()) {
+//                textview2.append("\nBluetooth is enabled...");
+//
+//                // Listing paired devices
+//                textview2.append("\nPaired Devices are:");
+//                Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
+//
+//                for (BluetoothDevice device : devices) {
+//                    textview2.append("\n  Device: " + device.getName() + ", " + device);
+//                }
+//            } else {
+//                //Prompt user to turn on Bluetooth
+//                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//            }
+//        }
+//    }
 
-                // Listing paired devices
-                textview2.append("\nPaired Devices are:");
-                Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
-                for (BluetoothDevice device : devices) {
-                    textview2.append("\n  Device: " + device.getName() + ", " + device);
+    private BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    private boolean scanning;
+    private Handler handler = new Handler();
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    private void scanLeDevice() {
+        if (!scanning) {
+            // Stops scanning after a predefined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    bluetoothLeScanner.stopScan(leScanCallback);
                 }
-            } else {
-                //Prompt user to turn on Bluetooth
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        } else {
+            scanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
+
+    private LeDeviceListAdapter leDeviceListAdapter = new LeDeviceListAdapter();
+
+    // Device scan callback.
+    private ScanCallback leScanCallback =
+            new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    leDeviceListAdapter.addDevice(result.getDevice());
+                    leDeviceListAdapter.notifyDataSetChanged();
+                }
+            };
+
+    // Adapter for holding devices found through scanning.
+    private class LeDeviceListAdapter extends BaseAdapter {
+        private ArrayList<BluetoothDevice> mLeDevices;
+        private LayoutInflater mInflator;
+        public LeDeviceListAdapter() {
+            super();
+            mLeDevices = new ArrayList<BluetoothDevice>();
+            mInflator = DeviceScanActivity.this.getLayoutInflater();
+        }
+        public void addDevice(BluetoothDevice device) {
+            if(!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
             }
+        }
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+        public void clear() {
+            mLeDevices.clear();
+        }
+        @Override
+        public int getCount() {
+            return mLeDevices.size();
+        }
+        @Override
+        public Object getItem(int i) {
+            return mLeDevices.get(i);
+        }
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = mInflator.inflate(R.layout.listitem_device, null);
+                viewHolder = new ViewHolder();
+                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+            BluetoothDevice device = mLeDevices.get(i);
+            final String deviceName = device.getName();
+            if (deviceName != null && deviceName.length() > 0)
+                viewHolder.deviceName.setText(deviceName);
+            else
+                viewHolder.deviceName.setText(R.string.unknown_device);
+            viewHolder.deviceAddress.setText(device.getAddress());
+            return view;
         }
     }
 
@@ -77,5 +182,10 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    static class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
     }
 }
